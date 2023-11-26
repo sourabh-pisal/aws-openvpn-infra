044fcc1c61d2bdf8593d6c23820c69c3ea37abbf
Sourabh Pisal <pisal.sourabh@gmail.com>
2023-11-26 22:28:07 +0000

    Adding Openvpn infra stack

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,16 +1,57 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import {
+  CfnEIP,
+  Instance,
+  InstanceClass,
+  InstanceSize,
+  InstanceType,
+  IpAddresses,
+  MachineImage,
+  Peer,
+  Port,
+  SecurityGroup,
+  SubnetType,
+  Vpc,
+} from "aws-cdk-lib/aws-ec2";
+import { Construct } from "constructs";
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class CdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, "vpc", {
+      ipAddresses: IpAddresses.cidr("10.0.0.0/24"),
+      natGateways: 0,
+      maxAzs: 1,
+      subnetConfiguration: [
+        {
+          name: "public-subnet",
+          subnetType: SubnetType.PUBLIC,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const sg = new SecurityGroup(this, "sg", {
+      vpc: vpc,
+      securityGroupName: "OpenVpnEc2",
+      allowAllOutbound: true,
+    });
+
+    sg.addIngressRule(Peer.anyIpv4(), Port.tcp(22), "SSH Port");
+    sg.addIngressRule(Peer.anyIpv4(), Port.tcp(443), "HTTPS Web Interface");
+    sg.addIngressRule(Peer.anyIpv4(), Port.tcp(943), "OpenVPN Webserver");
+    sg.addIngressRule(Peer.anyIpv4(), Port.udp(1194), "OpenVPN Reserved Port");
+
+    const ec2 = new Instance(this, "ec2", {
+      vpc: vpc,
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.SMALL),
+      machineImage: MachineImage.genericLinux({
+        "eu-west-1": "ami-0b541b356beff91e0",
+      }),
+      securityGroup: sg,
+      keyName: "openvpn",
+    });
+
+    new CfnEIP(this, "eip", { instanceId: ec2.instanceId });
   }
 }
